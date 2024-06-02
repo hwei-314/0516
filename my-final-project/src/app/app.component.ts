b97db11da232de3bb7236fa5e203a922639d4ec5
hwei-314 <waynewu0314@gmail.com>
2024-06-02 16:37:05 +0800

    change

--- my-final-project/src/app/app.component.ts
+++ my-final-project/src/app/app.component.ts
@@ -1,10 +1,18 @@
+import { BlogComponent } from './blog/blog.component';
+import { AboutComponent } from './about/about.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { PostComponent } from './blog/post/post.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, AboutComponent, BlogComponent, ContactComponent, PostComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
